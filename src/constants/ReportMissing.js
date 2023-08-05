59bc326cb9300d8f180ca6ca49c9bc96d6de12b1
inung1004 <dksdbswl10430@naver.com>
2023-08-06 00:49:39 +0900

    더미값 추가

---
+++ src/constants/ReportMissing.js
@@ -0,0 +1,44 @@
+export const datas = [
+  {
+    name: "정소라",
+    gender: "여",
+    age: 6,
+    character: "물감 묻히고 다님, 항상 웃음",
+    img: "https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_640.jpg",
+  },
+  {
+    name: "정소라",
+    gender: "여",
+    age: 6,
+    character: "물감 묻히고 다님, 항상 웃음",
+    img: "https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_640.jpg",
+  },
+  {
+    name: "정소라",
+    gender: "남",
+    age: 6,
+    character: "물감 묻히고 다님, 항상 웃음!!!",
+    img: "https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_640.jpg",
+  },
+  {
+    name: "정소라",
+    gender: "남",
+    age: 6,
+    character: "물감 묻히고 다님, 항상 웃음!!!",
+    img: "https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_640.jpg",
+  },
+  {
+    name: "정소라",
+    gender: "남",
+    age: 6,
+    character: "물감 묻히고 다님, 항상 웃음!!!",
+    img: "https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_640.jpg",
+  },
+  {
+    name: "정소라",
+    gender: "남",
+    age: 6,
+    character: "물감 묻히고 다님, 항상 웃음!!!",
+    img: "https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_640.jpg",
+  },
+];
