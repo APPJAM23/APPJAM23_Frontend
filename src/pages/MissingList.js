63055a05eebd7e457ac4d4808d7e1650597eb531
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-08-06 02:28:59 +0900

    route,  navigate 경로 수정

--- src/pages/MissingList.js
+++ src/pages/MissingList.js
@@ -1,27 +1,41 @@
 import { styled } from "styled-components";
 import { theme } from "../style/Theme";
+import { useNavigate } from "react-router-dom";
+import Dropdown from "../components/Dropdown";
 
 const datas = [
     {
+        id: 0,
         name: "정소라",
         gender: "여",
         age: 6,
         character: "물감 묻히고 다님, 항상 웃음",
-        img: 'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_640.jpg'
+        img: 'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_640.jpg',
+        height: 128,
+        weight: 34,
+        missing_date: '2023.06.07',
     },
     {
+        id: 1,
         name: "정소라",
         gender: "여",
         age: 6,
         character: "물감 묻히고 다님, 항상 웃음",
-        img: 'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_640.jpg'
+        img: 'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_640.jpg',
+        height: 128,
+        weight: 34,
+        missing_date: '2023.06.07',
     },
     {
+        id: 2,
         name: "정소라",
         gender: "남",
         age: 6,
-        character: "물감 묻히고 다님, 항상 웃음!!!",
-        img: 'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_640.jpg'
+        character: "물감 묻히고 다님, 항상 웃음",
+        img: 'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_640.jpg',
+        height: 128,
+        weight: 34,
+        missing_date: '2023.06.07',
     },
 ]
 
@@ -31,8 +45,13 @@ const text = theme.text;
 const MissingList = () => {
     return(
         <Container>
-            <Title>실종자 리스트</Title>
-            <SubTitle>함께 찾아주세요.</SubTitle>
+            <div style={{display: 'flex', gap: '60px'}}>
+                <div>
+                    <Title style={text.title01}>실종자 리스트</Title>
+                    <SubTitle style={text.subTitle}>함께 찾아주세요.</SubTitle>
+                </div>
+                <Dropdown />
+            </div>
             <ListContainer>
                 {datas.map((data) => {
                     return <List data={data}/>
@@ -43,39 +62,39 @@ const MissingList = () => {
 }
 
 const List = ({data}) => {
+    const navigate = useNavigate();
+
     return(
-        <div>
+        <div onClick={()=>{navigate(`detail/${data.id}`, {state : data})}}>
             <Image img={data.img} />
             <div style={{display: 'flex', gap: '60px', margin: '8px 0'}}>
-                <Name>{data.name}</Name>
+                <Name style={text.subTitle}>{data.name}</Name>
                 <div style={{display: 'flex', gap: '12px', alignItems: 'center'}}>
-                    <Gender gender={data.gender} >{data.gender}</Gender>
-                    <Age>{data.age}세</Age>
+                    <Gender style={text.body01} gender={data.gender} >{data.gender}</Gender>
+                    <Age style={text.body01}>{data.age}세</Age>
                 </div>
             </div>
-            <Char>{data.character}</Char>
+            <Char style={text.caption02}>{data.character}</Char>
         </div>
     );
 };
 
 const Container = styled.div`
-  margin: 18px 20px;
-`;
+    margin: 18px 20px;
+`
 const Title = styled.div`
     margin: 5px 0;
-    font-size: ${text.title01}px;
     // font-weight: bold;
-    color: ${colors.gray900};
+    font-weight: 600;
 `
 const SubTitle = styled.div`
-    font-size: ${text.body01}px;
     color: ${colors.gray500};
     margin-bottom: 21px;
 `;
 const ListContainer = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr;
-    grid-gap: 16px;
+    grid-gap: 16px 12px;
     margin: 16px 0;
 `;
 const Image = styled.div`
@@ -86,19 +105,17 @@ const Image = styled.div`
     border-radius: 6px;
 `
 const Name = styled.div`
-    font-size: ${text.subTitle}px;
     color: ${colors.gray900};
+    font-weight: 600;
 `
 const Gender = styled.span`
-    font-size: ${text.body01}px;
     color: ${props => props.gender === "여" ? "#f44336" : "#257cff"} // 남, 여
 `
 const Age = styled.span`
-    font-size: ${text.body01}px;
     color: ${colors.gray600};
 `
 const Char = styled.div`
-    font-size: ${text.caption02}px;
     color: ${colors.gray500};
 `
+export {Gender};
 export default MissingList;
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import MissingList from "../pages/MissingList";
 import TabLayout from "../pages/TabLayout";
 import MissingReportPage from "../pages/MissingReportPage";
+import MissingDetail from "../pages/MissingDetail";
 import MissingReportDetailPage from "../pages/MissingReportDetailPage";
 
 const MainRouter = () => {
@@ -10,6 +11,7 @@ const MainRouter = () => {
       <Route element={<TabLayout />}>
         {/* 탭이 들어가야 하는 페이지들은 여기에 :) */}
         <Route element={<MissingList />} path="/home" />
+        <Route element={<MissingDetail />} path="/home/detail/:id" />
         <Route element={<MissingReportPage />} path="/missing_report" />
         <Route element={<MissingReportDetailPage />} path="/report_detail" />
       </Route>
