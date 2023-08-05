e00d34b8c591948dc2a269b8f68ef41fc6311482
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-08-05 21:36:16 +0900

    실종자 리스트 페이지

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import MainPage from "../pages/Main";
+import MissingList from "../pages/MissingList";
 
 const MainRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
+      <Route path="/missinglist" element={<MissingList />} />
     </Routes>
   );
 };
